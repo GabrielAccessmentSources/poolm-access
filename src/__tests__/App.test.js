d9a7fcc8a9a61eddd58cce3a304800cad9ca9d5a
gabrielsaviank <gabrielsvnzrd@gmail.com>
2024-01-27 18:13:23 -0300

    feat: added RTK, Apollo and EsLint configs

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,7 +4,6 @@ import App from '../App';
 test('renders App component', () => {
   render(<App />);
 
-  // Find the element with the text "Hello World"
   const textElement = screen.getByText("Hello World");
 
   expect(textElement).toBeInTheDocument();
