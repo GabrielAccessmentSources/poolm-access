c7d3ea84331189dcd484f540c9023ade44d5ddd1
Gabriel Savian <gabrielsvnzrd@gmail.com>
2024-01-28 11:24:24 -0300

    feat: data is being fetched from the API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFilms } from "./ducks/actions/fetchFilms";
 
 import { Container } from "@mui/material";
 
-function App() {
+const App = () => {
+    const dispatch=  useDispatch();
+    const films = useSelector((state) => state.films.films);
+    const status = useSelector((state) => state.films.status);
+
+    useEffect(() => {
+        if(status === "idle"){
+            dispatch(fetchFilms());
+        }
+    }, [dispatch, status]);
+
+    console.log(films);
+
   return (
     <div className="App">
         <Container>
@@ -10,6 +24,6 @@ function App() {
         </Container>
     </div>
   );
-}
+};
 
 export default App;
